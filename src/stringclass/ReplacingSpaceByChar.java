5395f1e93ce7083fd02b4af89b8018c852f4b9e0
Anjali <anjalibobate12@gmail.com>
2024-05-17 15:01:15 +0530

    commiting change of the class seperateCharNo

--- src/stringclass/ReplacingSpaceByChar.java
+++ src/stringclass/ReplacingSpaceByChar.java
@@ -7,25 +7,23 @@ public class ReplacingSpaceByChar {
 		String s ="We will rock you";
 		String n ="";
 		char b;
-		int length = s.length();
+	//	int length = s.length();
 		
-		for(int i = 0; i < length; i++)
+		for(int i = 0; i < s.length(); i++)
 		{
 			char a = s.charAt(i);
 			if (a==' ')
 			{
-				char c = '_';
-				//b=c;
-				//a=b;
-				//n=n+a;
-				n=n+c;
+				
+			
+				n=n+'_';
 			}
 			else
 			{
 			n=n+s.charAt(i);
 			}
 		}
-		System.out.println(n);
+		System.out.print(n);
 	
 	}
 }
--- src/stringclass/SeperateCharNo.java
+++ src/stringclass/SeperateCharNo.java
@@ -6,11 +6,13 @@ public static void main(String[] args)
 	String s1= "abcd1234ghjkli8376";
 	String s3="";
 	String s4="";
-	boolean s2 = true;
+//	boolean s2 = true;
 	for(int i=0; i<s1.length();i++)
 	{
 		char c = s1.charAt(i);
-		if( s2 == Character.isAlphabetic(c))
+		
+	//	if( s2 == Character.isAlphabetic(c))
+		if( true == Character.isAlphabetic(c))
 		{
 			 s3 = s3 +  s1.charAt(i);
 				
